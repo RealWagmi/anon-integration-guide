194b5474c5a94d4415422b3b1adde64901602baf
Aaron <190254037+otfen@users.noreply.github.com>
2025-01-16 22:21:27 +1030

    feat: export functions

---
+++ projects/compound/functions/index.ts
@@ -0,0 +1,18 @@
+export { getAPRForAllMarkets } from './collateral/getAPRForAllMarkets';
+export { getAPRForMarket } from './collateral/getAPRForMarket';
+export { getBorrowedForAllMarkets } from './collateral/getBorrowedForAllMarkets';
+export { getBorrowedForMarket } from './collateral/getBorrowedForMarket';
+export { getCollateralForMarket } from './collateral/getCollateralForMarket';
+export { getSuppliedForAllMarkets } from './collateral/getSuppliedForAllMarkets';
+export { getSuppliedForMarket } from './collateral/getSuppliedForMarket';
+
+export { repay } from './supply/repay';
+export { repayAll } from './supply/repayAll';
+export { supplyBase } from './supply/supplyBase';
+export { supplyBaseAll } from './supply/supplyBaseAll';
+export { supplyCollateral } from './supply/supplyCollateral';
+
+export { borrow } from './withdraw/borrow';
+export { withdrawBase } from './withdraw/withdrawBase';
+export { withdrawBaseAll } from './withdraw/withdrawBaseAll';
+export { withdrawCollateral } from './withdraw/withdrawCollateral';
