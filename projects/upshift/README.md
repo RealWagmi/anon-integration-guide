0364be3d2aabfb5f7a346a461519928ac3855108
cunnil-eth <140001619+cunnil-eth@users.noreply.github.com>
2025-01-23 10:21:44 +0300

    Add files via upload

---
+++ projects/upshift/README.md
@@ -0,0 +1,54 @@
+# Upshift
+
+Integration with Upshift
+
+
+## Overview
+
+Upshift is a digital storefront for on-chain yield, powered by August Digital.
+It offers retail users simplified access to native (“Lend”) and Partner vaults (“VaaS”).
+
+## Features
+
+Key functions of Upshift:
+- Deposit into vaults and redeem assets
+- Stake and unstake AVAX and AUSD for extra APY
+- Claim rewards and deposits
+- Fetch vault's APY
+- Fetch vault's TVL
+- Fetch vault's additional rewards
+
+## Supported Networks
+
+- ETHEREUM
+- BASE
+- AVALANCHE
+
+## Common Tasks
+
+1. Basic Operations
+   - "Deposit 800 LBTC"
+   - "Redeem 50 wstETH"
+   - "Claim 10 sUSDe"
+   - "Stake 100 upAVAX"
+   - "Unstake 333 upAUSD"
+   - "Redeem upAVAX rewards"
+
+2. Information Queries
+   - "Show current APY for LBTC on Base in Upshift"
+   - "Check wstETH TVL in upshift"
+   - "Show rewards in AVAX vault in @upshift"
+
+
+## Addressing Pain Points
+
+- Simplified User Experience - A single reference token per vault reduces complexity for users.
+- The receipt token allows for use cases on secondary markets, such as Pendle.
+- Multi-Chain Yield Opportunities - Vaults natively support multiple chains, allowing users to benefit from diverse DeFi yield programs without needing manual bridging.
+- Security & Permissions - Strategists manage vaults but are limited by strict permissions and whitelisting controls.
+
+## Installation
+
+```bash
+yarn add @heyanon/upshift
+```
---
+++ projects/upshift/tools.ts
@@ -0,0 +1,217 @@
+import { AiTool, getChainName } from '@heyanon/sdk';
+import { supportedChains } from './constants';
+
+export const tools: AiTool[] = [
+    {
+        name: 'claimAsset',
+        description: 'Claims asset from the Upshift vault after requesting redeem',
+        required: ['chainName', 'account', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where to execute claim',
+            },
+            {
+                name: 'account',
+                type: 'string',
+                description: 'Account address that will execute the claim',
+            },
+            {
+                name: 'amount',
+                type: 'string',
+                description: 'Asset that will be claimed',
+            },
+        ],
+    },
+    {
+        name: 'depositAsset',
+        description: 'Deposits asset into the Upshift vault',
+        required: ['chainName', 'account', 'amount', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where to execute deposit',
+            },
+            {
+                name: 'account',
+                type: 'string',
+                description: 'Account address that will execute the deposit',
+            },
+            {
+                name: 'amount',
+                type: 'string',
+                description: 'Amount of tokens that will be deposited',
+            },
+            {
+                name: 'token',
+                type: 'string',
+                description: 'Asset that will be deposited',
+            },
+        ],
+    },
+    {
+        name: 'requestRedeemAsset',
+        description: 'Requests redeem of an asset from the Upshift vault',
+        required: ['chainName', 'account', 'amount', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where to execute redeem request',
+            },
+            {
+                name: 'account',
+                type: 'string',
+                description: 'Account address that will execute the redeem request',
+            },
+            {
+                name: 'amount',
+                type: 'string',
+                description: 'Amount of tokens that will be redeemed',
+            },
+            {
+                name: 'token',
+                type: 'string',
+                description: 'Asset that will be redeemed',
+            },
+        ],
+    },
+    {
+        name: 'stakeOnAvalanche',
+        description: 'Stakes AVAX or AUSD into the Upshift vault on Avalanche network',
+        required: ['chainName', 'account', 'amount', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where to execute stake',
+            },
+            {
+                name: 'account',
+                type: 'string',
+                description: 'Account address that will execute the stake',
+            },
+            {
+                name: 'amount',
+                type: 'string',
+                description: 'Amount of tokens that will be staked',
+            },
+            {
+                name: 'token',
+                type: 'string',
+                description: 'Asset that will be staked',
+            },
+        ],
+    },
+    {
+        name: 'unstakeOnAvalanche',
+        description: 'Unstakes AVAX or AUSD from the Upshift vault on Avalanche network',
+        required: ['chainName', 'account', 'amount', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where to execute unstake',
+            },
+            {
+                name: 'account',
+                type: 'string',
+                description: 'Account address that will execute the unstake',
+            },
+            {
+                name: 'amount',
+                type: 'string',
+                description: 'Amount of tokens that will be unstaked',
+            },
+            {
+                name: 'token',
+                type: 'string',
+                description: 'Asset that will be unstaked',
+            },
+        ],
+    },
+    {
+        name: 'redeemRewardOnAvalanche',
+        description: 'Redeems rewards from Upshift LP on Avalanche network',
+        required: ['chainName', 'account', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where to execute reward redeem',
+            },
+            {
+                name: 'account',
+                type: 'string',
+                description: 'Account address that will execute the redeem',
+            },
+            {
+                name: 'token',
+                type: 'string',
+                description: 'Asset vault that will be redeem rewards from',
+            },
+        ],
+    },
+    {
+        name: 'getVaultApy',
+        description: 'Fetchs APY (annual percentage yield) for a given vault',
+        required: ['chainName', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where vault is deployed',
+            },
+            {
+                name: 'token',
+                type: 'string',
+                description: 'Underlying asset for a vault to fetch APY from',
+            },
+        ],
+    },
+    {
+        name: 'getVaultRewards',
+        description: 'Fetchs additional rewards (points) for a given vault',
+        required: ['chainName', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where vault is deployed',
+            },
+            {
+                name: 'token',
+                type: 'string',
+                description: 'Underlying asset for a vault to fetch rewards from',
+            },
+        ],
+    },
+    {
+        name: 'getVaultTvl',
+        description: 'Fetchs TLV (total value locked) for a given vault',
+        required: ['chainName', 'token'],
+        props: [
+            {
+                name: 'chainName',
+                type: 'string',
+                enum: supportedChains.map(getChainName),
+                description: 'Chain name where vault is deployed',
+            },
+            {
+                name: 'token',
+                type: 'string',
+                description: 'Underlying asset for a vault to fetch TVL from',
+            },
+        ],
+    },
+];
