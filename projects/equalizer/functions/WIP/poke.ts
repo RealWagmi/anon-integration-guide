a54e15452316726217a3bb20b1f222096e15edc2
kinsyudev <miguel@kinsyu.dev>
2025-01-31 07:30:23 +0100

    poke token id

--- projects/equalizer/functions/WIP/poke.ts
+++ projects/equalizer/functions/WIP/poke.ts
@@ -1,23 +1,20 @@
 import { Address, encodeFunctionData } from 'viem';
 import { FunctionReturn, FunctionOptions, TransactionParams, toResult, getChainFromName } from '@heyanon/sdk';
-import { supportedChains } from '../../constants';
+import { supportedChains, VOTER_ADDRESS } from '../../constants';
 import { voterAbi } from '../../abis/voterAbi';
 
 interface Props {
     chainName: string;
     account: Address;
-    voterAddress: Address;
     tokenId: string;
 }
 
-export async function poke({ chainName, account, voterAddress, tokenId }: Props, { sendTransactions, notify }: FunctionOptions): Promise<FunctionReturn> {
+export async function poke({ chainName, account, tokenId }: Props, { sendTransactions, notify }: FunctionOptions): Promise<FunctionReturn> {
     if (!account) return toResult('Wallet not connected', true);
 
     const chainId = getChainFromName(chainName);
     if (!chainId) return toResult(`Unsupported chain name: ${chainName}`, true);
     if (!supportedChains.includes(chainId)) return toResult(`Equalizer is not supported on ${chainName}`, true);
-
-    if (!voterAddress) return toResult('Voter address is required', true);
 
     const tokenIdBn = BigInt(tokenId);
 
@@ -26,7 +23,7 @@ export async function poke({ chainName, account, voterAddress, tokenId }: Props,
     const transactions: TransactionParams[] = [];
 
     const pokeTx: TransactionParams = {
-        target: voterAddress,
+        target: VOTER_ADDRESS,
         data: encodeFunctionData({
             abi: voterAbi,
             functionName: 'poke',
