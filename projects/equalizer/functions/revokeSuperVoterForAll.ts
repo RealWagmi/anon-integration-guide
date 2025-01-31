dfa127566a2d2a71cc86095dca2dc241c5a9d98f
kinsyudev <miguel@kinsyu.dev>
2025-01-31 07:31:30 +0100

    integrated revoke super voter for all

--- projects/equalizer/functions/revokeSuperVoterForAll.ts
+++ projects/equalizer/functions/revokeSuperVoterForAll.ts
@@ -1,38 +1,30 @@
 import { Address, encodeFunctionData } from 'viem';
 import { FunctionReturn, FunctionOptions, TransactionParams, toResult, getChainFromName } from '@heyanon/sdk';
-import { supportedChains } from '../../constants';
-import { veNftAbi } from '../../abis/veNftAbi';
+import { SUPER_VOTER_ADDRESS, supportedChains, VE_EQUAL_ADDRESS } from '../constants';
+import { veNftAbi } from '../abis/veNftAbi';
 
 interface Props {
     chainName: string;
     account: Address;
-    vestedAddress: Address;
-    superVoterAddress: Address;
 }
 
-export async function revokeSuperVoterForAll(
-    { chainName, account, vestedAddress, superVoterAddress }: Props,
-    { sendTransactions, notify }: FunctionOptions
-): Promise<FunctionReturn> {
+export async function revokeSuperVoterForAll({ chainName, account }: Props, { sendTransactions, notify }: FunctionOptions): Promise<FunctionReturn> {
     if (!account) return toResult('Wallet not connected', true);
 
     const chainId = getChainFromName(chainName);
     if (!chainId) return toResult(`Unsupported chain name: ${chainName}`, true);
     if (!supportedChains.includes(chainId)) return toResult(`Equalizer is not supported on ${chainName}`, true);
 
-    if (!vestedAddress) return toResult('Vested address is required', true);
-    if (!superVoterAddress) return toResult('Super Voter address is required', true);
-
     await notify('Preparing to revoke Super Voter approval...');
 
     const transactions: TransactionParams[] = [];
 
     const revokeTx: TransactionParams = {
-        target: vestedAddress,
+        target: VE_EQUAL_ADDRESS,
         data: encodeFunctionData({
             abi: veNftAbi,
             functionName: 'setApprovalForAll',
-            args: [superVoterAddress, false],
+            args: [SUPER_VOTER_ADDRESS, false],
         }),
     };
     transactions.push(revokeTx);
