e38c30e0e67498407febacad6aeb429e8214074e
SarveshLimaye <sarveshlimaye2002@gmail.com>
2025-02-03 09:34:48 +0530

    chore:protocol description updated

--- projects/eddy-finance/index.ts
+++ projects/eddy-finance/index.ts
@@ -5,5 +5,6 @@ import * as functions from './functions';
 export default {
     tools,
     functions,
-    description: 'Integration with Eddy Finance',
+    description:
+        'Eddy Finance is a universal cross-chain DEX and a liquidity layer, enabling seamless movement of native assets across both EVM and non-EVM chains like Bitcoin, Ethereum, Solana, Base, and more—without the need for wrapping.Eddy uses threshold signing schemes and a unique universal smart router that executes advanced algorithms like concentrated liquidity and stableswap algos across chains reducing slippage and gas costs',
 } satisfies AdapterExport;
