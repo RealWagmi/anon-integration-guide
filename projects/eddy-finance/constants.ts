a90bec9449ff280d02a20a5093cb610baf598829
SarveshLimaye <sarveshlimaye2002@gmail.com>
2025-02-03 11:51:52 +0530

    feat:bridge quote

--- projects/eddy-finance/constants.ts
+++ projects/eddy-finance/constants.ts
@@ -15,6 +15,17 @@ export const USDT_ZRC20 = '0x7c8dda80bbbe1254a7aacf3219ebe1481c6e01d7';
 export const ETH_ZRC20 = '0xd97b1de3619ed2c6beb3860147e30ca8a7dc9891';
 export const PEPE_ZRC20 = '0x236b0de675cc8f46ae186897fccefe3370c9eded';
 export const DAI_ZRC20 = '0xcc683a782f4b30c138787cb5576a86af66fdc31d';
+export const EDDY_GRAPHQL_URL = 'https://apiv2.eddy.finance/graphql';
+
+interface BridgeQuoteResponse {
+    estimatedRecievedAmount: string;
+    quoteAmount: string;
+    minimumReceived: string;
+}
+
+export const RETRIES = 3;
+
+export const DELAY = 1000;
 
 export const getZRC20Address = (destTokenSymbol: string) => {
     switch (destTokenSymbol.toLowerCase()) {
@@ -54,4 +65,58 @@ export const getDataForBitcoin = (btcWalletAddress: string) => {
 export const getNativeTokenName = (chainId: number) => {
     const chain = [mainnet, bsc, polygon, base].find((c) => c.id === chainId);
     return chain?.nativeCurrency.symbol || 'Not supported';
+};
+
+export const fetchBridgeQuote = async (
+    inputTokenAddress: string,
+    outputTokenAddress: string,
+    inputAmount: string,
+    slippage: number,
+    fromChainId: number,
+    toChainId: number,
+): Promise<BridgeQuoteResponse> => {
+    const query = `
+    query {
+      getBridgeQuoteByTokenAddress(
+        bridgeQuoteInput: {
+          inputTokenAddress: "${inputTokenAddress}"
+          outputTokenAddress: "${outputTokenAddress}"
+          inputAmount: "${inputAmount}"
+          slippage: ${slippage}
+          fromChainId: ${fromChainId}
+          toChainId: ${toChainId}
+        }
+      ) {
+        estimatedRecievedAmount
+        quoteAmount
+        minimumReceived
+      }
+    }
+  `;
+
+    try {
+        const response = await fetch(EDDY_GRAPHQL_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify({ query }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        if (!result.data) {
+            throw new Error(`Invalid response: ${JSON.stringify(result)}`);
+        }
+
+        return result.data.getBridgeQuoteByTokenAddress;
+    } catch (error) {
+        console.error('Error fetching bridge quote:', error);
+        throw error;
+    }
 };
