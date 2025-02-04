d57f149bcdd21b00da64215f3079d3dc6c6fdb2f
lowkeys <dheerajhasija1992@gmail.com>
2025-02-04 22:59:44 +0530

    Checking borrow limits before borrow.

--- projects/venus/functions/borrowToken.ts
+++ projects/venus/functions/borrowToken.ts
@@ -1,26 +1,21 @@
-import {
-    Address,
-    encodeFunctionData, parseUnits,
-} from "viem";
+import { Address, encodeFunctionData, parseUnits, formatUnits } from 'viem';
 
-import {
-    FunctionReturn,
-    FunctionOptions,
-    TransactionParams,
-    toResult,
-} from "@heyanon/sdk";
+import { FunctionReturn, FunctionOptions, TransactionParams, toResult } from '@heyanon/sdk';
 
-import {vBNBAbi} from "../abis/vBNBAbi";
-import {validateAndGetTokenDetails, validateWallet} from "../utils";
+import { vBNBAbi } from '../abis/vBNBAbi';
+import { validateAndGetTokenDetails, validateWallet } from '../utils';
+import { vComptrollerAbi } from '../abis/vComptrollerAbi';
+import { vOrcaleABI } from '../abis/vOracleABI';
+import { ORACLE_ADDRESS } from '../constants';
+import { vTokenAbi } from '../abis/vTokenAbi';
 
 interface Props {
     chainName: string;
     account: Address;
     amount: string;
-    token: string;
+    tokenSymbol: string;
     pool: string;
 }
-
 
 /**
  * Borrows Token using Venus protocol.
@@ -29,11 +24,8 @@ interface Props {
  * @param tools - System tools for blockchain interactions.
  * @returns Borrow result containing the transaction hash.
  */
-export async function borrowToken(
-    {chainName, account, amount, token, pool}: Props,
-    {sendTransactions, notify}: FunctionOptions
-): Promise<FunctionReturn> {
-    const wallet = validateWallet({account})
+export async function borrowToken({ chainName, account, amount, tokenSymbol, pool }: Props, { sendTransactions, notify, getProvider }: FunctionOptions): Promise<FunctionReturn> {
+    const wallet = validateWallet({ account });
     if (!wallet.success) {
         return toResult(wallet.errorMessage, true);
     }
@@ -41,18 +33,48 @@ export async function borrowToken(
         return toResult('Invalid amount', true);
     }
     // Validate chain
-    const tokenDetails = validateAndGetTokenDetails({chainName, pool, tokenSymbol: token})
+    const tokenDetails = validateAndGetTokenDetails({ chainName, pool, tokenSymbol: tokenSymbol });
     if (!tokenDetails.success) {
         return toResult(tokenDetails.errorMessage, true);
     }
+    const provider = getProvider(tokenDetails.data.chainId);
     try {
-        await notify("Preparing to borrow Token...");
+        await notify('Checking the borrow limit of the account...');
+        const [shortfall, liquidity] = await provider.readContract({
+            abi: vComptrollerAbi,
+            address: tokenDetails.data.poolAddress,
+            functionName: 'getAccountLiquidity',
+            args: [account],
+        });
+        //Always 18
+        const borrowLimitInUSD = parseFloat(formatUnits(liquidity, 18)).toFixed(2);
+        if (borrowLimitInUSD <= 0) {
+            return toResult('No available liquidity to borrow. Please supply a collateral', true);
+        }
+        const underlyingAssetAddress = await provider.readContract({
+            abi: vTokenAbi,
+            address: tokenDetails.data.tokenAddress,
+            functionName: 'underlying',
+            args: [],
+        });
+
+        const tokenPriceInUSD = await provider.readContract({
+            abi: vOrcaleABI,
+            address: ORACLE_ADDRESS,
+            functionName: 'getPrice',
+            args: [underlyingAssetAddress],
+        });
+        if (borrowLimitInUSD <= parseFloat(formatUnits(tokenPriceInUSD, 18)).toFixed(2) * amount) {
+            return toResult('Not enough borrow limit please supply more', true);
+        }
+
+        await notify('Preparing to borrow Token...');
         // Prepare borrow transaction
         const borrowTx: TransactionParams = {
             target: tokenDetails.data.tokenAddress,
             data: encodeFunctionData({
                 abi: vBNBAbi,
-                functionName: "borrow",
+                functionName: 'borrow',
                 args: [parseUnits(amount, 18)],
             }),
         };
@@ -63,11 +85,8 @@ export async function borrowToken(
             transactions: [borrowTx],
         });
         const borrowMessage = result.data[result.data.length - 1];
-        return toResult(result.isMultisig ? borrowMessage.message : `Successfully borrowed ${amount} ${token}.`);
+        return toResult(result.isMultisig ? borrowMessage.message : `Successfully borrowed ${amount} ${tokenSymbol}.`);
     } catch (error) {
-        return toResult(
-            `Failed to borrow token: ${error instanceof Error ? error.message : "Unknown error"}`,
-            true
-        );
+        return toResult(`Failed to borrow token: ${error instanceof Error ? error.message : 'Unknown error'}`, true);
     }
 }
