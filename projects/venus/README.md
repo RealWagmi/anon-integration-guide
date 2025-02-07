111268becb3a8a7e21d4ff0d9fd017e98ad5f13b
lowkeys <dheerajhasija1992@gmail.com>
2025-02-07 09:14:30 +0530

    Fixed comments

--- projects/venus/README.md
+++ projects/venus/README.md
@@ -14,4 +14,14 @@ Venus Protocol is a decentralized lending and borrowing protocol that enables us
 - Borrowing rate per block
 
 ### Supported Networks
-- Mainnet
+- BSC
+- ETHEREUM
+- BASE
+
+### Examples
+- Supply 1 BNB to Venus core pool.
+- Enter market for BNB in core pool.
+- Enable BNB as collateral for core pool.
+- Borrow 10 USDT from Venus
+- Borrow 10 USDC from Venus Defi pool
+- Repay 10 USDT to the core pool.
--- projects/venus/functions/borrowBalanceCurrentToken.ts
+++ projects/venus/functions/borrowBalanceCurrentToken.ts
@@ -1,19 +1,11 @@
-import {
-    Address,
-    encodeFunctionData,
-} from "viem";
+import { Address, encodeFunctionData } from 'viem';
 
-import {
-    FunctionReturn,
-    FunctionOptions,
-    TransactionParams,
-    toResult,
-} from "@heyanon/sdk";
+import { FunctionReturn, FunctionOptions, TransactionParams, toResult } from '@heyanon/sdk';
 
-
-import {vBNBAbi} from "../abis/vBNBAbi";
-import {validateAndGetTokenDetails, validateWallet} from "../utils";
-
+import { vBNBAbi } from '../abis/vBNBAbi';
+import { validateAndGetTokenDetails, validateWallet } from '../utils';
+import { vComptrollerAbi } from '../abis/vComptrollerAbi';
+import { vTokenAbi } from '../abis/vTokenAbi';
 
 interface Props {
     chainName: string;
@@ -22,29 +14,31 @@ interface Props {
     pool: string;
 }
 
-
 /**
  * Retrieves the borrow balance of token from the Venus protocol.
  *
  * @returns {Promise<FunctionReturn>} - The borrow balance of Token.
  */
-export async function borrowBalanceCurrentToken({chainName, account, tokenSymbol, pool}: Props,
-                                                {sendTransactions, notify,}: FunctionOptions): Promise<FunctionReturn> {
-    const wallet = validateWallet({account})
+export async function borrowBalanceCurrentToken(
+    { chainName, account, tokenSymbol, pool }: Props,
+    { sendTransactions, notify, getProvider }: FunctionOptions,
+): Promise<FunctionReturn> {
+    const wallet = validateWallet({ account });
     if (!wallet.success) {
         return toResult(wallet.errorMessage, true);
     }
-    const tokenDetails = validateAndGetTokenDetails({chainName, pool, tokenSymbol: tokenSymbol})
+    const tokenDetails = validateAndGetTokenDetails({ chainName, pool, tokenSymbol: tokenSymbol });
     if (!tokenDetails.success) {
         return toResult(tokenDetails.errorMessage, true);
     }
+    const provider = getProvider(tokenDetails.data.chainId);
     try {
-        await notify("Preparing transaction...");
+        await notify('Preparing transaction...');
         const borrowBalanceCurrent: TransactionParams = {
             target: tokenDetails.data.tokenAddress,
             data: encodeFunctionData({
                 abi: vBNBAbi,
-                functionName: "borrowBalanceCurrent",
+                functionName: 'borrowBalanceCurrent',
                 args: [wallet.data.account],
             }),
         };
@@ -54,12 +48,17 @@ export async function borrowBalanceCurrentToken({chainName, account, tokenSymbol
             account,
             transactions: [borrowBalanceCurrent],
         });
+
+        const borrowBalanceStored = await provider.readContract({
+            abi: vTokenAbi,
+            address: tokenDetails.data.tokenAddress,
+            functionName: 'borrowBalanceStored',
+            args: [account],
+        });
+
         const borrowMessage = result.data[result.data.length - 1];
-        return toResult(result.isMultisig ? borrowMessage.message : `Successfully fetched borrow balance of ${tokenSymbol}.`);
+        return toResult(result.isMultisig ? borrowMessage.message : `Successfully updated borrow balance of ${tokenSymbol} updated balance ${borrowBalanceStored}.`);
     } catch (error) {
-        return toResult(
-            `Failed to redeem token: ${error instanceof Error ? error.message : "Unknown error"}`,
-            true
-        );
+        return toResult(`Failed to redeem token: ${error instanceof Error ? error.message : 'Unknown error'}`, true);
     }
 }
