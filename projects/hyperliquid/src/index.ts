ab04ac4bd00cab6117c3ecaf02066babeaace3b5
vaniiiii <ivandzambas@gmail.com>
2025-03-05 18:07:53 -0500

    refactor: Add functions export

--- projects/hyperliquid/src/index.ts
+++ projects/hyperliquid/src/index.ts
@@ -1,8 +1,10 @@
 import { AdapterExport } from '@heyanon/sdk';
 import { tools } from './tools';
+import * as functions from './functions';
 
 export default {
-	tools,
-	functions: {},
-	description: 'An example project that demonstrates how to create a new project.',
+    tools,
+    functions,
+    description:
+        'Hyperliquid is Layer 1 blockchain providing CEX-like trading experience through on-chain order books. Enables USDC bridging and supports both spot/perpetual trading with full position management',
 } satisfies AdapterExport;
