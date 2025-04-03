500101f669133ad1b46d0895ef7e761d9d899733
Guido W. Pettinari <coccoinomane@gmail.com>
2025-04-03 18:47:02 +0200

    refac: change beefy cli folder name

--- projects/beefy/tsup.config.ts
+++ projects/beefy/tsup.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig((options) => ({
     entry: {
         index: './src/index.ts',
-        'beefy-cli/index': './src/beefy-cli/index.ts',
+        'beefy_cli/index': './src/beefy_cli/index.ts',
         run: './src/ask_beefy/run.ts',
     },
     format: ['esm', 'cjs'],
