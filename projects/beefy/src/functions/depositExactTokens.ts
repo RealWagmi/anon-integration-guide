cdd2c924da3b167046d8df8a03889bcb799732c6
Guido W. Pettinari <coccoinomane@gmail.com>
2025-04-09 16:05:24 +0200

    refac: helper function buildtExactTokensTransactions

--- projects/beefy/src/functions/depositExactTokens.ts
+++ projects/beefy/src/functions/depositExactTokens.ts
@@ -1,11 +1,7 @@
-import { Address, encodeFunctionData, parseUnits, erc20Abi } from 'viem';
+import { Address } from 'viem';
 import { FunctionReturn, FunctionOptions, toResult, EVM, EvmChain } from '@heyanon/sdk';
 import { supportedChains } from '../constants';
-import { getBeefyChainNameFromAnonChainName } from '../helpers/chains';
-import { getSimplifiedVaultByIdAndChain } from '../helpers/vaults';
-import { beefyVaultAbi } from '../abis';
-import { toHumanReadableAmount } from '../helpers/format';
-import { getTokenInfoFromAddress } from '../helpers/tokens';
+import { buildtExactTokensTransactions } from '../helpers/deposit';
 import { TokenInfo } from '../helpers/beefyClient';
 
 interface Props {
@@ -31,95 +27,27 @@ interface Props {
  * @param {FunctionOptions} context - Holds EVM utilities and a notifier
  * @returns {Promise<FunctionReturn>} A message confirming the deposit or an error description
  */
-export async function depositExactTokens(
-    { chainName, account, vaultId, amount, tokenAddress }: Props,
-    { evm: { sendTransactions, getProvider }, notify }: FunctionOptions,
-): Promise<FunctionReturn> {
+export async function depositExactTokens({ chainName, account, vaultId, amount, tokenAddress }: Props, options: FunctionOptions): Promise<FunctionReturn> {
     const chainId = EVM.utils.getChainFromName(chainName as EvmChain);
     if (!chainId) return toResult(`Unsupported chain name: ${chainName}`, true);
     if (!supportedChains.includes(chainId)) return toResult(`Beets protocol is not supported on ${chainName}`, true);
 
-    const provider = getProvider(chainId);
-
-    // Fetch the vault info
-    const beefyChainName = getBeefyChainNameFromAnonChainName(chainName);
-    const vault = await getSimplifiedVaultByIdAndChain(vaultId, beefyChainName);
-    if (!vault) return toResult(`Could not find vault with ID ${vaultId}`, true);
-
-    // Compute the amount in wei
-    const amountInWei = parseUnits(amount, vault.depositedTokenDecimals);
-    if (amountInWei === 0n) return toResult('Amount must be greater than 0', true);
-
-    // Get token address from vault contract
-    // We could get it from the API (as vault.depositedTokenAddress), but
-    // sometimes (e.g. native chain tokens) the API omits it
-    const depositedTokenAddress = (await provider.readContract({
-        address: vault.vaultContractAddress as `0x${string}`,
-        abi: beefyVaultAbi,
-        functionName: 'want',
-    })) as `0x${string}`;
-    if (!depositedTokenAddress) return toResult('Could not get token address from vault contract', true);
-
-    let tokenInfo: TokenInfo;
+    // Build the transactions that need to be sent
+    let transactions: EVM.types.TransactionParams[] = [];
     let depositedTokenInfo: TokenInfo;
     try {
-        tokenInfo = await getTokenInfoFromAddress(chainName, tokenAddress);
-        depositedTokenInfo = await getTokenInfoFromAddress(chainName, depositedTokenAddress);
+        [transactions, depositedTokenInfo] = await buildtExactTokensTransactions(account, chainName, vaultId, amount, tokenAddress, options);
     } catch (error) {
-        return toResult('Could not get info on token to deposit', true);
+        return toResult(error instanceof Error ? error.message : 'An unknown error occurred', true);
     }
-
-    // Check that the user is trying to deposit the right token
-    if (tokenAddress.toLowerCase() !== depositedTokenAddress.toLowerCase()) {
-        return toResult('You are trying to deposit the wrong token.  The vault wants ' + depositedTokenInfo.id + ' but you are trying to deposit ' + tokenInfo.symbol, true);
-    }
-
-    await notify(`Checking your balance of ${depositedTokenInfo.id}...`);
-
-    // Check user balance
-    let balance: bigint;
-    balance = await provider.readContract({
-        address: depositedTokenAddress as `0x${string}`,
-        abi: erc20Abi,
-        functionName: 'balanceOf',
-        args: [account],
-    });
-    if (balance < amountInWei) {
-        let msg = `Not enough tokens: you need ${amount} ${vault.depositedTokenSymbol} but you have ${toHumanReadableAmount(balance, vault.depositedTokenDecimals)}`;
-        if (vault.depositedTokenUrl) {
-            msg += `\nVisit this URL to acquire the tokens: ${vault.depositedTokenUrl}`;
-        }
-        return toResult(msg);
-    }
-
-    // Maybe approve token to vault
-    const transactions: EVM.types.TransactionParams[] = [];
-    await EVM.utils.checkToApprove({
-        args: { account, target: depositedTokenAddress, spender: vault.vaultContractAddress, amount: amountInWei },
-        provider,
-        transactions,
-    });
-
-    await notify(`Preparing to deposit ${amount} ${depositedTokenInfo.id} into vault ${vault.name}...`);
-
-    // Prepare deposit transaction
-    const tx: EVM.types.TransactionParams = {
-        target: vault.vaultContractAddress as `0x${string}`,
-        data: encodeFunctionData({
-            abi: beefyVaultAbi,
-            functionName: 'deposit',
-            args: [amountInWei],
-        }),
-    };
-    transactions.push(tx);
 
     if (transactions.length === 1) {
-        await notify('Sending deposit transaction...');
+        await options.notify('Sending deposit transaction...');
     } else if (transactions.length > 1) {
-        await notify('Sending approval & deposit transactions...');
+        await options.notify('Sending approval & deposit transactions...');
     }
 
-    const result = await sendTransactions({ chainId, account, transactions });
+    const result = await options.evm.sendTransactions({ chainId, account, transactions });
     const message = result.data[result.data.length - 1].message;
     return toResult(result.isMultisig ? message : `Successfully deposited ${amount} ${depositedTokenInfo.id}. ${message}`);
 }
---
+++ projects/beefy/src/helpers/deposit.ts
@@ -0,0 +1,103 @@
+import { encodeFunctionData, parseUnits, erc20Abi, Address } from 'viem';
+import { FunctionOptions, EVM, EvmChain } from '@heyanon/sdk';
+import { getBeefyChainNameFromAnonChainName } from '../helpers/chains';
+import { getSimplifiedVaultByIdAndChain } from '../helpers/vaults';
+import { beefyVaultAbi } from '../abis';
+import { toHumanReadableAmount } from '../helpers/format';
+import { getTokenInfoFromAddress } from '../helpers/tokens';
+import { TokenInfo } from '../helpers/beefyClient';
+
+/**
+ * Deposit the specified amount of tokens into a vault.  Both the token to
+ * deposit and the vault contract address are determined by the vaultId.
+ *
+ * Docs: https://docs.beefy.finance/developer-documentation/vault-contract
+ */
+export async function buildtExactTokensTransactions(
+    account: Address,
+    chainName: string,
+    vaultId: string,
+    amount: string,
+    tokenAddress: `0x${string}`,
+    { evm: { getProvider }, notify }: FunctionOptions,
+): Promise<[EVM.types.TransactionParams[], TokenInfo]> {
+    // Get provider
+    const chainId = EVM.utils.getChainFromName(chainName as EvmChain);
+    const provider = getProvider(chainId);
+
+    // Fetch the vault info
+    const beefyChainName = getBeefyChainNameFromAnonChainName(chainName);
+    const vault = await getSimplifiedVaultByIdAndChain(vaultId, beefyChainName);
+    if (!vault) throw new Error(`Could not find vault with ID ${vaultId}`);
+
+    // Compute the amount in wei
+    const amountInWei = parseUnits(amount, vault.depositedTokenDecimals);
+    if (amountInWei === 0n) throw new Error('Amount must be greater than 0');
+
+    // Get token address from vault contract
+    // We could get it from the API (as vault.depositedTokenAddress), but
+    // sometimes (e.g. native chain tokens) the API omits it
+    const depositedTokenAddress = (await provider.readContract({
+        address: vault.vaultContractAddress as `0x${string}`,
+        abi: beefyVaultAbi,
+        functionName: 'want',
+    })) as `0x${string}`;
+    if (!depositedTokenAddress) throw new Error('Could not get token address from vault contract');
+
+    // Get info on the token the user wants to deposit (tokenInfo) and on
+    // the token the vault wants (depositedTokenInfo)
+    let tokenInfo: TokenInfo;
+    let depositedTokenInfo: TokenInfo;
+    try {
+        tokenInfo = await getTokenInfoFromAddress(chainName, tokenAddress);
+        depositedTokenInfo = await getTokenInfoFromAddress(chainName, depositedTokenAddress);
+    } catch (error) {
+        throw new Error('Could not get info on token to deposit');
+    }
+
+    // Check that the user is trying to deposit the right token
+    if (tokenAddress.toLowerCase() !== depositedTokenAddress.toLowerCase()) {
+        throw new Error('You are trying to deposit the wrong token.  The vault wants ' + depositedTokenInfo.id + ' but you are trying to deposit ' + tokenInfo.symbol);
+    }
+
+    await notify(`Checking your balance of ${depositedTokenInfo.id}...`);
+
+    // Check user balance
+    let balance: bigint;
+    balance = await provider.readContract({
+        address: depositedTokenAddress as `0x${string}`,
+        abi: erc20Abi,
+        functionName: 'balanceOf',
+        args: [account],
+    });
+    if (balance < amountInWei) {
+        let msg = `Not enough tokens: you need ${amount} ${vault.depositedTokenSymbol} but you have ${toHumanReadableAmount(balance, vault.depositedTokenDecimals)}`;
+        if (vault.depositedTokenUrl) {
+            msg += `\nVisit this URL to acquire the tokens: ${vault.depositedTokenUrl}`;
+        }
+        throw new Error(msg);
+    }
+
+    // Maybe approve token to vault
+    const transactions: EVM.types.TransactionParams[] = [];
+    await EVM.utils.checkToApprove({
+        args: { account, target: depositedTokenAddress, spender: vault.vaultContractAddress, amount: amountInWei },
+        provider,
+        transactions,
+    });
+
+    await notify(`Preparing to deposit ${amount} ${depositedTokenInfo.id} into vault ${vault.name}...`);
+
+    // Prepare deposit transaction
+    const tx: EVM.types.TransactionParams = {
+        target: vault.vaultContractAddress as `0x${string}`,
+        data: encodeFunctionData({
+            abi: beefyVaultAbi,
+            functionName: 'deposit',
+            args: [amountInWei],
+        }),
+    };
+    transactions.push(tx);
+
+    return [transactions, depositedTokenInfo];
+}
