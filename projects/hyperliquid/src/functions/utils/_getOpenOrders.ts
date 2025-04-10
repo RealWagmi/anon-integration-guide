943afe8a02fb063bb28675d7f7058bfafb81f718
Skocimis <aki.spremo@gmail.com>
2025-04-10 14:50:34 +0200

    feat: Fetching the list of open orders for user

---
+++ projects/hyperliquid/src/functions/utils/_getOpenOrders.ts
@@ -0,0 +1,43 @@
+import { Address } from 'viem';
+import WebSocket from 'ws';
+
+export async function _getOpenOrders(userAddress: Address): Promise<any> {
+    return new Promise((resolve) => {
+        const ws = new WebSocket('wss://api.hyperliquid.xyz/ws');
+
+        const subscriptionMessage = {
+            method: 'subscribe',
+            subscription: {
+                type: 'webData2',
+                user: userAddress,
+            },
+        };
+
+        ws.on('open', () => {
+            ws.send(JSON.stringify(subscriptionMessage));
+        });
+
+        ws.on('message', (data: WebSocket.Data) => {
+            try {
+                const parsedData = JSON.parse(data.toString());
+                if (parsedData?.data && parsedData?.channel == 'webData2') {
+                    resolve(parsedData?.data?.openOrders || []);
+                    ws.close();
+                }
+            } catch (err) {
+                console.error('Error parsing message:', err);
+                resolve(null);
+                ws.close();
+            }
+        });
+
+        ws.on('error', (err: Error) => {
+            console.error('WebSocket error:', err);
+            resolve(null);
+        });
+
+        ws.on('close', () => {
+            // Optionally handle close
+        });
+    });
+}
