b6b53af8b3b2bbba88698e67a60d8f37bb40e5e9
Guido W. Pettinari <coccoinomane@gmail.com>
2025-05-22 18:40:20 +0200

    feat: exchange agnostic ask agent

---
+++ projects/bybit/src/agent/tools.ts
@@ -0,0 +1,8 @@
+import { AiTool } from '@heyanon/sdk';
+
+/**
+ * Tools specific for the agent, and that are not
+ * included in the HeyAnon integration because they could
+ * interfer with HeyAnon's functionality.
+ */
+export const tools: AiTool[] = [];
---
+++ projects/bybit/src/constants.ts
@@ -0,0 +1,37 @@
+// ---------------------------------------------------------------
+// GLOBAL CONSTANTS
+// ---------------------------------------------------------------
+// UX and functional constants that are not exchange specific
+// ---------------------------------------------------------------
+
+/**
+ * Maximum number of markets to show in results,
+ * important to avoid the 500 token limit for getters.
+ */
+export const MAX_MARKETS_IN_RESULTS = 50;
+
+/**
+ * Maximum number of orders to show in results,
+ * important to avoid the 500 token limit for getters.
+ */
+export const MAX_ORDERS_IN_RESULTS = 25;
+
+/**
+ * Maximum number of positions to show in results,
+ * important to avoid the 500 token limit for getters.
+ */
+export const MAX_POSITIONS_IN_RESULTS = 25;
+
+/**
+ * By default, attempts of sending a limit order that will execute immediately
+ * will be rejected.  This tolerance allows for a small deviation from the
+ * current price.  Set to 0 to block all limit orders that will execute immediately.
+ * Set to a few permille to allow for some deviation that might happens due to
+ * current price fluctuations while the order is being placed.
+ */
+export const LIMIT_PRICE_TOLERANCE = 0.005;
+
+/**
+ * Possible account types supported by CCXT
+ */
+export const ACCOUNT_TYPES = ['main', 'spot', 'funding', 'margin', 'cross', 'future', 'delivery', 'linear', 'swap', 'inverse', 'option'] as const;
